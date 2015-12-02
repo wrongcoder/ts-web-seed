afb0dc8764cf38c9232471ccf9c202c231538030
wrongcoder <git@badcod.ca>
2015-12-02 04:16:27 -0800

    Bundle application for distribution

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,8 +4,10 @@ module.exports = function (grunt) {
 	grunt.loadNpmTasks("grunt-bowercopy");
 	grunt.loadNpmTasks("grunt-contrib-clean");
 	grunt.loadNpmTasks("grunt-contrib-connect");
+	grunt.loadNpmTasks("grunt-contrib-uglify");
 	grunt.loadNpmTasks("grunt-mkdir");
 	grunt.loadNpmTasks("grunt-ts");
+	grunt.loadNpmTasks("grunt-webpack");
 
 	grunt.registerTask("default", ["clean", "mkdir", "ts", "bowercopy", "connect:dev:keepalive"]);
 
@@ -37,6 +39,38 @@ module.exports = function (grunt) {
 					"require1k.js": "require1k/require1k.js",
 					"less.js": "less/dist/less.js",
 				},
+			},
+		},
+		webpack: {
+			dist: {
+				devtool: "source-map",
+				entry: "./build/tsc/app/Application",
+				output: {
+					path: "build/bundle",
+					filename: "bundle.js",
+					sourceMapFilename: "bundle.js.map",
+				},
+				module: {
+					preLoaders: [
+						{ test: /\.js$/, loader: "source-map" },
+					],
+				},
+			},
+		},
+		uglify: {
+			dist: {
+				src: ["build/bundle/bundle.js"],
+				dest: "build/bundle/bundle.min.js",
+			},
+			options: {
+				now: new Date(),
+				mangle: {
+					toplevel: true,
+				},
+				compress: true,
+				sourceMap: true,
+				sourceMapIn: ["build/bundle/bundle.js.map"],
+				banner: "/* \u00A9 <%= uglify.options.now.getFullYear() %> example.com */",
 			},
 		},
 		connect: {

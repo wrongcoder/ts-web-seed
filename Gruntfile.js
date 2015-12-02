cf31f1fab0f72c4d060463cbb830715e442be6c3
wrongcoder <git@badcod.ca>
2015-12-01 22:19:14 -0800

    Add basic TypeScript compilation step

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,31 @@
 module.exports = function (grunt) {
 	"use strict";
 
-	grunt.registerTask("default", []);
+	grunt.loadNpmTasks("grunt-contrib-clean");
+	grunt.loadNpmTasks("grunt-mkdir");
+	grunt.loadNpmTasks("grunt-ts");
+
+	grunt.registerTask("default", ["clean", "mkdir", "ts"]);
 
 	grunt.initConfig({
+		clean: {
+			default: ["build"],
+		},
+		mkdir: {
+			default: {
+				options: {
+					create: ["build/tsc"],
+				},
+			},
+		},
+		ts: {
+			default: {
+				tsconfig: {
+					tsconfig: "src",
+					passThrough: true,
+				},
+			},
+		},
 	});
 
 };
